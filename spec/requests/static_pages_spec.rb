429358198fd41d661f280b4fb35b2503d0d87e30
K Sichien <ksichien@posteo.net>
2017-08-11 14:46:13 +0200

    Refactored static pages spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,22 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
   include Devise::Test::IntegrationHelpers
-  describe "GET /home" do
+  describe "GET /home before log in" do
     it "redirects to users sign in page" do
       get root_path
       expect(response).to redirect_to(new_user_session_path)
       expect(response).to have_http_status(302)
     end
-    it "retrieves the home page" do
+  end
+  describe "GET /home after log in" do
+    before(:each) do
       user = build(:jd)
       sign_in user
+    end
+    it "retrieves the home page" do
       get root_path
       expect(response).to be_successful
       expect(response).to have_http_status(200)
     end
     it "displays the home template" do
-      user = build(:jd)
-      sign_in user
       get root_path
       expect(response).to render_template("home")
     end
