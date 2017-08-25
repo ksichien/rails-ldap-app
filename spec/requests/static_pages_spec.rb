e7d0221225da7a00c6273d2f26f6d59fde1def1c
K Sichien <ksichien@posteo.net>
2017-08-25 12:33:04 +0200

    minor name changes in static pages request spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "StaticPages", type: :request do
   include Devise::Test::IntegrationHelpers
   describe "GET /home before log in" do
-    it "redirects to users sign in page" do
+    it "redirects to new user session path" do
       get root_path
       expect(response).to redirect_to(new_user_session_path)
       expect(response).to have_http_status(302)
