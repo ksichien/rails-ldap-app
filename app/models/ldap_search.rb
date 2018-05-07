444811969ca47de666ab14243f3aca7cd8df9052
ksichien <ksichien@posteo.net>
2018-05-07 16:59:31 +0200

    added string processing for search results

--- app/models/ldap_search.rb
+++ app/models/ldap_search.rb
@@ -15,7 +15,13 @@ class LdapSearch
     ldap = ldap_login(ldap_username, ldap_password)
     filter = Net::LDAP::Filter.eq('cn', "*#{name}*")
     ldap.search(base: SERVERDC, filter: filter, return_result: true) do |entry|
-      result << "#{entry.dn}\n"
+      if entry.dn.include? USEROU
+        result << "User name: #{entry.dn.gsub! ",#{USEROU},#{SERVERDC}", ''}\n"
+      elsif entry.dn.include? GROUPOU
+        result << "Group name: #{entry.dn.gsub! ",#{GROUPOU},#{SERVERDC}", ''}\n"
+      else
+        result << "Unusual Object name: #{entry.dn}\n"
+      end
     end
     result << "Operation search directory for '#{name}' " \
               "result: #{ldap.get_operation_result.message}\n"
